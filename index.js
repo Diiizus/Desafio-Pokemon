eb68d20e25515af1c847c2619452c6c28483f12b
LuanSimões <luansimoes1212@gmail.com>
2024-11-29 09:30:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const nomeDoPokemon = [
+    ["Poochyena", "Lv2", "Masculino", "Hp 13/13"],
+    ["Zigzagoon", "Lv2", "Feminino", "Hp 13/13"],
+    ["Dragonite", "Lv5", "Masculino", "Hp 25/25"],
+    ["Dragonite", "Lv5", "Feminino", "Hp 24/24"],
+    ["Dragonite", "Lv5", "Feminino", "Hp 24/24"],
+    ["Poochyena", "Lv3", "Feminino", "Hp 15/15"],
+    ["Hurmple", "Lv2", "Masculino", "Hp 7/14"]
+]
+
+console.log ("Você capturou com sucesso o Pokemon " + nomeDoPokemon[0][0] + " com o " + nomeDoPokemon[0][1] + " do sexo " + nomeDoPokemon[0][2] + " com o " + nomeDoPokemon[0][3] + " !")
+console.log ("Você capturou com sucesso o Pokemon " + nomeDoPokemon[1][0] + " com o " + nomeDoPokemon[1][1] + " do sexo " + nomeDoPokemon[1][2] + " com o " + nomeDoPokemon[1][3] + " !")
+console.log ("Você capturou com sucesso o Pokemon " + nomeDoPokemon[2][0] + " com o " + nomeDoPokemon[2][1] + " do sexo " + nomeDoPokemon[2][2] + " com o " + nomeDoPokemon[2][3] + " !")
+console.log ("Você capturou com sucesso o Pokemon " + nomeDoPokemon[3][0] + " com o " + nomeDoPokemon[3][1] + " do sexo " + nomeDoPokemon[3][2] + " com o " + nomeDoPokemon[3][3] + " !")
+console.log ("Você capturou com sucesso o Pokemon " + nomeDoPokemon[4][0] + " com o " + nomeDoPokemon[4][1] + " do sexo " + nomeDoPokemon[4][2] + " com o " + nomeDoPokemon[4][3] + " !")
+console.log ("Você capturou com sucesso o Pokemon " + nomeDoPokemon[5][0] + " com o " + nomeDoPokemon[5][1] + " do sexo " + nomeDoPokemon[5][2] + " com o " + nomeDoPokemon[5][3] + " !")
+console.log ("Você capturou com sucesso o Pokemon " + nomeDoPokemon[6][0] + " com o " + nomeDoPokemon[6][1] + " do sexo " + nomeDoPokemon[6][2] + " com o " + nomeDoPokemon[6][3] + " !")
